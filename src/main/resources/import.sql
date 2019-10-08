148d45e8cc8d0ce073c3e6ed4f8d1bd8e9279801
yehanportilla <jancha22@gmail.com>
2019-10-08 07:07:55 -0500

    Se realiza encriptacion de contraseña en import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','1234',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','12345',1, 'yehan', 'portilla','yehanpor@ceiba.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$gn39tZSaI28rQWMGkURZke/gWhxolP2niDWQN0hzLVZuxvuM3xb2S',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$8Z1q7SCMSo5w.y6TuJ0nludJI8ja6Q8Ud2YuD.b0ZfAo2v//GVmOa',1, 'yehan', 'portilla','yehanpor@ceiba.com');
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
