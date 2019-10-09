4b63d4e39ceccf00a7cddefe41d2b13a76ecba9e
yehanportilla <jancha22@gmail.com>
2019-10-09 14:51:27 -0500

    Se agrega dependencia de driver postgres y se edita el archivo .sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$gn39tZSaI28rQWMGkURZke/gWhxolP2niDWQN0hzLVZuxvuM3xb2S',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$8Z1q7SCMSo5w.y6TuJ0nludJI8ja6Q8Ud2YuD.b0ZfAo2v//GVmOa',1, 'yehan', 'portilla','yehanpor@ceiba.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$gn39tZSaI28rQWMGkURZke/gWhxolP2niDWQN0hzLVZuxvuM3xb2S',true, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$8Z1q7SCMSo5w.y6TuJ0nludJI8ja6Q8Ud2YuD.b0ZfAo2v//GVmOa',true, 'yehan', 'portilla','yehanpor@ceiba.com');
 
-INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
-INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
 
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
-INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 2);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 1);
